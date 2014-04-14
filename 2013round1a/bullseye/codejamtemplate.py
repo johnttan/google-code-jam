aadb619287b8d4f3c21b63ca74cbab5994b7669c
John Tan <tantai.john@gmail.com>
2014-04-14 17:50:51 -0500

    Created casesolver class.

---
+++ 2013round1a/bullseye/codejamtemplate.py
@@ -0,0 +1,32 @@
+from helperclasses import FileLoader
+import os
+from solve import Classcasesolver
+
+config = {
+    "name": os.getcwd().split(os.sep)[-1],
+    "casefilepath": "tests/D-large.in",
+    "split": False
+}
+# FileLoader attributes:
+#     self.file
+#     self.casesnumber
+#     self.casesdeque
+
+files = FileLoader(config["name"], config["casefilepath"])
+
+
+
+############################Loads case################################
+def caseloader(file):
+    return
+############################Loads case################################
+
+#Initializes case solving class.
+#Classcasesolver.casesolve(case) method takes case and calls necessary functions to solve it. Returns solution.
+casesolver = Classcasesolver()
+files.loadcustom(caseloader)
+files.solve(casesolver.casesolve, True)
+#Case number input is 1 less than real case number. 0 indexed deque.
+# files.solvesingle(40, casesolver, True)
+
+print()
---
+++ 2013round1a/bullseye/solve.py
@@ -0,0 +1,7 @@
+"""
+Functions to solve case
+"""
+
+class Classcasesolver(object):
+    def casesolve(self, case):
+        return
